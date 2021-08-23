5824013d656e1bb9a819dda3748ac94952f1a2ab
kenta0313 <asanokenta@me.com>
2021-08-23 11:43:40 +0900

    ✨ 検索機能追加

--- src/graphql/queries.ts
+++ src/graphql/queries.ts
@@ -1,8 +1,8 @@
 import gql from "graphql-tag"
 
-export const searchPikachu = gql`
-    query searchPikachu {
-        pokemon(name: "pikachu") {
+export const searchPokemon = gql`
+    query searchPokemon($name: String) {
+        pokemon(name: $name) {
             number
             name
             image
